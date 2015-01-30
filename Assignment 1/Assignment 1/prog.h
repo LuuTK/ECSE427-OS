d97889e31c4037cf633438f00bbfc43cb348c557
TKLuu <tuan-kiet.luu@mail.mcgill.ca>
2015-01-29 21:31:13 -0500

    Initial Commit

---
+++ Assignment 1/Assignment 1/prog.h
@@ -0,0 +1,14 @@
+//
+//  prog.h
+//  Assignment 1
+//
+//  Created by Tuan Kiet Luu on 2015-01-29.
+//
+//
+
+#ifndef __Assignment_1__prog__
+#define __Assignment_1__prog__
+
+#include <stdio.h>
+
+#endif /* defined(__Assignment_1__prog__) */
